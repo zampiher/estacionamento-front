47d25baa24395b7eb48af872f285d099e83d5e9a
zampiher <seuemail@exemplo.com>
2026-06-09 19:02:57 -0300

    criando o front

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Users from "./pages/Users";
+
+function App() {
+  return <Users />;
+}
+
+export default App;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,5 @@
+function Users() {
+  return <h1>Usuários</h1>;
+}
+
+export default Users;
